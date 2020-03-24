a9c93d2d084baa0b81c08c89bd9e819143d15273
allstendres98 <allstendres@users.noreply.github.com>
2020-03-24 17:03:09 +0100

    adapter
    no sale nada pero no da error

--- app/src/main/java/com/al375502/ujisoccer/ListTeamActivity.java
+++ app/src/main/java/com/al375502/ujisoccer/ListTeamActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.al375502.ujisoccer.database.TeamInStanding;
@@ -13,7 +14,8 @@ import java.util.ArrayList;
 public class ListTeamActivity extends AppCompatActivity {
 
     public static final String LEAGUE = "League";
-    TextView funciono;
+    ListView listView;
+    ArrayList<TeamInStanding> standingsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +24,19 @@ public class ListTeamActivity extends AppCompatActivity {
         Intent intent = getIntent();
         int league_id = intent.getIntExtra(LEAGUE,2021);
 
-        funciono = findViewById(R.id.textView);
+        listView = findViewById(R.id.listView);
+        standingsList = new ArrayList<>();
 
         final ListTeamPresenter presenter = new ListTeamPresenter(this, Model.getInstance(getApplicationContext()));
         presenter.GetStandings(league_id);
 
+        MyAdapter adapter = new MyAdapter(this, standingsList);
+        listView.setAdapter(adapter);
+
     }
 
     public void FillListView(ArrayList<TeamInStanding> response) {
-        funciono. setText("position: " +response.get(0).position+ " nombre: " + response.get(0).name + " playedGames: " + response.get(0).playedGames);
+        //funciono. setText("position: " +response.get(0).position+ " nombre: " + response.get(0).name + " playedGames: " + response.get(0).playedGames);
+        standingsList = response;
     }
 }
---
+++ app/src/main/java/com/al375502/ujisoccer/MyAdapter.java
@@ -0,0 +1,64 @@
+package com.al375502.ujisoccer;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.al375502.ujisoccer.database.TeamInStanding;
+
+import java.util.ArrayList;
+
+public class MyAdapter extends BaseAdapter {
+    Context context;
+    ArrayList<TeamInStanding> arr;
+
+    public MyAdapter(Context context, ArrayList<TeamInStanding> arr) {
+        this.context = context;
+        this.arr = arr;
+    }
+
+    @Override
+    public int getCount() {
+        return arr.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        convertView = LayoutInflater.from(context).inflate(R.layout.rowdesign,parent,false);
+
+        TextView posT = (TextView) convertView.findViewById(R.id.posT);
+        TextView teamT = (TextView) convertView.findViewById(R.id.teamT);
+        TextView plT = (TextView) convertView.findViewById(R.id.plT);
+        TextView wT = (TextView) convertView.findViewById(R.id.wT);
+        TextView dT = (TextView) convertView.findViewById(R.id.dT);
+        TextView lT = (TextView) convertView.findViewById(R.id.lT);
+        TextView ptT = (TextView) convertView.findViewById(R.id.ptT);
+        TextView gfT = (TextView) convertView.findViewById(R.id.gfT);
+        TextView gaT = (TextView) convertView.findViewById(R.id.gaT);
+
+        posT.setText(arr.get(position).position);
+        teamT.setText(arr.get(position).name);
+        plT.setText(arr.get(position).playedGames);
+        wT.setText(arr.get(position).won);
+        dT.setText(arr.get(position).draw);
+        lT.setText(arr.get(position).lost);
+        ptT.setText(arr.get(position).points);
+        gfT.setText(arr.get(position).goalsFor);
+        gaT.setText(arr.get(position).goalsAgainst);
+
+        return convertView;
+    }
+}
