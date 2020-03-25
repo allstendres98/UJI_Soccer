532fd7b25c4987136da9b27dafd915ad6dcc71ce
allstendres98 <allstendres@users.noreply.github.com>
2020-03-25 19:04:03 +0100

    web
    ya va la web

--- app/src/main/java/com/al375502/ujisoccer/ListTeamActivity.java
+++ app/src/main/java/com/al375502/ujisoccer/ListTeamActivity.java
@@ -2,13 +2,18 @@ package com.al375502.ujisoccer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.al375502.ujisoccer.database.Team;
 import com.al375502.ujisoccer.database.TeamInStanding;
@@ -29,6 +34,7 @@ public class ListTeamActivity extends AppCompatActivity {
         setContentView(R.layout.activity_list_team);
         Intent intent = getIntent();
         int league_id = intent.getIntExtra(LEAGUE,2021);
+        final ListTeamActivity thisView = this;
 
         listView = findViewById(R.id.listView);
 
@@ -38,20 +44,32 @@ public class ListTeamActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String teamSelected = "";
+                String teamSelectedS = "";
+                Team teamSelected;
                 for(int i = 0; i < teamInStandings.size(); i++)
                 {
                     if(teamInStandings.get(i).position == position+1){
-                        teamSelected = teamInStandings.get(i).name;
+                        teamSelectedS = teamInStandings.get(i).name;
                     }
                 }
 
-                Log.d("Equipo",teamSelected);
+                for(int i = 0; i < teams.size(); i++)
+                {
+                    //Log.d("Equipo","deberia de salir pero solo lo pensé");
+                    if(teamSelectedS.equals(teams.get(i).name))
+                    {
+                        teamSelected = teams.get(i);
+                        TeamDialog teamDialog = new TeamDialog(teamSelected, thisView);
+                        teamDialog.show(getSupportFragmentManager(),"team dialog");
+                    }
+                }
+
+                //
             }
         });
     }
 
-    //EL PROBLEMA ERA EL PUTO FillDataBaseWithTeams del Model, porque por alguna puta razon, el año de fundación de algunos equipos era null, y yo intentaba pillar entero y petaba jaja xd
+    //EL PROBLEMA ERA EL FillDataBaseWithTeams del Model, porque por alguna razon, el año de fundación de algunos equipos era null, y yo intentaba pillar entero y petaba jaja xd
 
     public void FillListView(ArrayList<TeamInStanding> response) {
         teamInStandings = response;
@@ -61,6 +79,21 @@ public class ListTeamActivity extends AppCompatActivity {
 
     public void PopUpTeam(ArrayList<Team> allteams) {
         teams = allteams;
+    }
+
+    public void GetIntoWebsite(String url)
+    {
+        /*WebView webView = new WebView(this);
+        setContentView(webView);
+        webView.loadUrl(teamWeb);*/
+        try {
+            Uri webpage = Uri.parse(url);
+            Intent myIntent = new Intent(Intent.ACTION_VIEW, webpage);
+            startActivity(myIntent);
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(this, "No application can handle this request. Please install a web browser or check your URL.",  Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
     }
     
 }
--- app/src/main/java/com/al375502/ujisoccer/TeamDialog.java
+++ app/src/main/java/com/al375502/ujisoccer/TeamDialog.java
@@ -2,9 +2,14 @@ package com.al375502.ujisoccer;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.webkit.WebView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatDialogFragment;
 
@@ -12,18 +17,58 @@ import com.al375502.ujisoccer.database.Team;
 
 public class TeamDialog extends AppCompatDialogFragment {
     public Team teamSelected;
+    public ListTeamActivity view;
+    public TextView sh, f, st, cc;
 
-    public TeamDialog(Team teamSelected) {
+
+    public TeamDialog(Team teamSelected, ListTeamActivity view) {
         this.teamSelected = teamSelected;
+        this.view = view;
     }
+
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
         LayoutInflater inflater = getActivity().getLayoutInflater();
-        View view = inflater.inflate(R.layout.team_dialog, null);
-        builder.setView(view).setTitle(teamSelected.name);
+        View viewInflate = inflater.inflate(R.layout.team_dialog, null);
+        builder.setView(viewInflate).setTitle(teamSelected.name);
+
+        builder.setPositiveButton("WEB", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                view.GetIntoWebsite(teamSelected.website);
+            }
+        });
+        builder.setNeutralButton("OKAY", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.setNegativeButton("NEXT MATCHES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        sh = viewInflate.findViewById(R.id.sh);
+        f = viewInflate.findViewById(R.id.f);
+        st = viewInflate.findViewById(R.id.st);
+        cc = viewInflate.findViewById(R.id.cc);
+
+            sh.setText(teamSelected.shortName + "");
+            f.setText(teamSelected.yearFoundation + "");
+            st.setText(teamSelected.stadium + "");
+            cc.setText(teamSelected.colours + "");
+
+
         return builder.create();
     }
+
+    /*public interface TeamDialogListener{
+
+    }*/
 }
