bc4430fed78a995fc9d86b36e1bdf734642c67af
allstendres98 <allstendres@users.noreply.github.com>
2020-03-25 19:22:59 +0100

    listview squad

---
+++ app/src/main/java/com/al375502/ujisoccer/SquadActivity.java
@@ -0,0 +1,14 @@
+package com.al375502.ujisoccer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SquadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_squad);
+    }
+}
